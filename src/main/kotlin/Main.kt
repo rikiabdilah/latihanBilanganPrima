9112ee9277613bee61479b6a95ab7abb0c14bca9
rikiabdilah <rikiabdillah2002@gmail.com>
2023-03-08 19:41:09 +0700

    latihan bilangan prima

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,23 +1,3 @@
-
 fun main() {
-    print("Masukkan Angka: ")
-    val tampung = readLine()?.toInt()?: 0
 
-    if (bilanganPrima(tampung)){
-        println("$tampung adalah bilangan prima")
-    } else {
-        println("$tampung bukan bilangan prima")
-    }
-}
-
-fun bilanganPrima(number: Int): Boolean{
-    if (number <= 2){
-        return false
-    }
-    for (i in 2..number / 2){
-        if (number % i == 0){
-            return false
-        }
-    }
-    return true
 }
